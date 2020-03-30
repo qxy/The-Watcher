f9824fef76b276fa7f8bdcc3b943b996b4493b6c
Krisztián Gesztiványi <qxy@qxy.hu>
2020-03-30 20:41:07 +0200

    cleanup default mess :)

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,103 +1,46 @@
 <template>
-  <q-layout view="lHh Lpr lFf">
-    <q-header elevated>
-      <q-toolbar>
-        <q-btn
-          flat
-          dense
-          round
-          icon="menu"
-          aria-label="Menu"
-          @click="leftDrawerOpen = !leftDrawerOpen"
-        />
+	<q-layout view="hHh lpr fFf">
+		<q-header elevated>
+			<q-bar class="q-electron-drag">
+				<div>TV Show Tracker</div>
+				<q-space />
+				<q-btn dense flat icon="minimize" @click="minimize" />
+				<q-btn dense flat icon="crop_square" @click="maximize" />
+				<q-btn dense flat icon="close" @click="closeApp" />
+			</q-bar>
+		</q-header>
 
-        <q-toolbar-title>
-          Quasar App
-        </q-toolbar-title>
-
-        <div>Quasar v{{ $q.version }}</div>
-      </q-toolbar>
-    </q-header>
-
-    <q-drawer
-      v-model="leftDrawerOpen"
-      show-if-above
-      bordered
-      content-class="bg-grey-1"
-    >
-      <q-list>
-        <q-item-label
-          header
-          class="text-grey-8"
-        >
-          Essential Links
-        </q-item-label>
-        <EssentialLink
-          v-for="link in essentialLinks"
-          :key="link.title"
-          v-bind="link"
-        />
-      </q-list>
-    </q-drawer>
-
-    <q-page-container>
-      <router-view />
-    </q-page-container>
-  </q-layout>
+		<q-page-container>
+			<router-view />
+		</q-page-container>
+	</q-layout>
 </template>
 
 <script>
-import EssentialLink from 'components/EssentialLink'
-
 export default {
-  name: 'MainLayout',
+	name: 'DefaultLayout',
+	methods: {
+		minimize() {
+			if (process.env.MODE === 'electron') {
+				this.$q.electron.remote.BrowserWindow.getFocusedWindow().minimize()
+			}
+		},
+		maximize() {
+			if (process.env.MODE === 'electron') {
+				const win = this.$q.electron.remote.BrowserWindow.getFocusedWindow()
 
-  components: {
-    EssentialLink
-  },
-
-  data () {
-    return {
-      leftDrawerOpen: false,
-      essentialLinks: [
-        {
-          title: 'Docs',
-          caption: 'quasar.dev',
-          icon: 'school',
-          link: 'https://quasar.dev'
-        },
-        {
-          title: 'Github',
-          caption: 'github.com/quasarframework',
-          icon: 'code',
-          link: 'https://github.com/quasarframework'
-        },
-        {
-          title: 'Discord Chat Channel',
-          caption: 'chat.quasar.dev',
-          icon: 'chat',
-          link: 'https://chat.quasar.dev'
-        },
-        {
-          title: 'Forum',
-          caption: 'forum.quasar.dev',
-          icon: 'record_voice_over',
-          link: 'https://forum.quasar.dev'
-        },
-        {
-          title: 'Twitter',
-          caption: '@quasarframework',
-          icon: 'rss_feed',
-          link: 'https://twitter.quasar.dev'
-        },
-        {
-          title: 'Facebook',
-          caption: '@QuasarFramework',
-          icon: 'public',
-          link: 'https://facebook.quasar.dev'
-        }
-      ]
-    }
-  }
+				if (win.isMaximized()) {
+					win.unmaximize()
+				} else {
+					win.maximize()
+				}
+			}
+		},
+		closeApp() {
+			if (process.env.MODE === 'electron') {
+				this.$q.electron.remote.BrowserWindow.getFocusedWindow().close()
+			}
+		}
+	}
 }
 </script>
